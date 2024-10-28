aba9befc0562dc938770eced5b0d54e88b27802b
Khỉ AI <khi.me@zohomail.com>
2024-10-28 09:15:52 +0700

    Update remove-youtube-shorts.user.js

--- remove-youtube-shorts.user.js
+++ remove-youtube-shorts.user.js
@@ -1,51 +1,39 @@
 // ==UserScript==
-// @name         Remove YouTube Shorts
+// @name         Remove YouTube Shorts and Rich Section
 // @namespace    https://github.com/khi-ai
-// @version      0.1
-// @description  Removes YouTube Shorts Videos
+// @version      0.2
+// @description  Removes YouTube Shorts Videos and Rich Section
 // @author       khi-ai
 // @match        http://*.youtube.com/*
 // @match        https://*.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
-(
-    () =>
-    {
-        const removeShorts = () =>
-        {
-            Array
-                .from(document.querySelectorAll(`a[href^="/shorts"]`))
-                .forEach
-                (
-                    a =>
-                    {
-                        let parent = a.parentElement;
 
-                        while (parent && (!parent.tagName.startsWith('YTD-') || parent.tagName === 'YTD-THUMBNAIL'))
-                        {
-                            parent = parent.parentElement;
-                        }
-
-                        if (parent)
-                        {
-                            parent.remove();
-                        }
-                    }
-                )
-            ;
-        }
-
-        const observer = new MutationObserver(removeShorts);
-        observer.observe
-        (
-            document,
-            {
-                childList:  true,
-                subtree:    true,
+(() => {
+    const removeElements = () => {
+        // Xóa các video YouTube Shorts
+        Array.from(document.querySelectorAll('a[href^="/shorts"]')).forEach(a => {
+            let parent = a.parentElement;
+            while (parent && (!parent.tagName.startsWith('YTD-') || parent.tagName === 'YTD-THUMBNAIL')) {
+                parent = parent.parentElement;
             }
-        );
+            if (parent) {
+                parent.remove();
+            }
+        });
 
-        removeShorts();
-    }
-)();
+        // Xóa phần tử #content trong ytd-rich-section-renderer
+        document.querySelectorAll('#content.ytd-rich-section-renderer').forEach(content => {
+            content.remove();
+        });
+    };
+
+    const observer = new MutationObserver(removeElements);
+    observer.observe(document, {
+        childList: true,
+        subtree: true,
+    });
+
+    removeElements();
+})();
